8f154dff2f783a6571211febd0869698ad86f8f8
Bret Fisher <bret@fishbrains.com>
2017-05-14 22:30:31 -0400

    removing pw

--- rocker/build.sh
+++ rocker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -f 73.Dockerfile --tag rocker --build-arg NONMEM_ZIP_PASS_73=gz952BqZX5 .
+docker build -f 73.Dockerfile --tag rocker --build-arg NONMEM_ZIP_PASS_73=ADDPASS .
